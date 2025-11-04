773999c688e006261f7862da559525530b513417
Daniel <baluga.aguilar323@gmail.com>
2025-11-04 13:52:03 -0800

    Initial Commit

---
+++ You Are Awsome/You_Are_AwsomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  You_Are_AwsomeApp.swift
+//  You Are Awsome
+//
+//  Created by AGUILAR, DANIEL on 11/4/25.
+//
+
+import SwiftUI
+
+@main
+struct You_Are_AwsomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
